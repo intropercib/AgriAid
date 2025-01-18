4681e6ea1d68eba8abc1f7e91f66e52c96bf96e1
intropercib <intropercib@gmail.com>
2025-01-18 21:23:25 +0545

    cure added feed fix

--- software/app.py
+++ software/app.py
@@ -5,18 +5,28 @@ from sensor_data import SensorData
 app = Flask(__name__)
 
 # Initialize classes for video processing and sensor data
-video_processor = VideoProcessor(video_url="http://192.168.18.7:8080/video")
-sensor_data = SensorData(port='COM5') 
+video_processor = VideoProcessor(video_url="http://192.168.18.7:8080/video", model_path="models/model.h5")
+sensor_data = SensorData(port='COM5', baudrate=9600, timeout=1) 
+
+def generate_video_feed():
+    for frame in video_processor.generate_frames():
+        yield frame
 
 @app.route('/')
 def index():
     """Render the main page."""
     return render_template('index.html')
 
+@app.route('/cure')
+def cure():
+    """Route for the cure page."""
+    return render_template('cure.html')
+
 @app.route('/video_feed')
 def video_feed():
     """Route for the live video feed."""
-    return Response(video_processor.generate_frames(),
+    print("Accessing video feed...")
+    return Response(generate_video_feed(),
                     mimetype='multipart/x-mixed-replace; boundary=frame')
 
 @app.route('/sensor_data')
@@ -27,4 +37,5 @@ def get_sensor_data():
     return data
 
 if __name__ == '__main__':
-    app.run(debug=True)
+
+    app.run(debug=False)
